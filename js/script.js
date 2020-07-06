aab5d5ccd71a1a6eec9b99d97a0a91db2e47e2e4
Hyunmin Kim <landiajuve@gmail.com>
2020-07-06 11:04:06 +0900

    0706수정2.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // 이미지 슬라이드
 
 // 각 변수들을 선언.
-var currentSlide = Number($('.slider__slide--active').data('slide')),
-    totalSlides = $('.slider__slide').length, // 전체 슬라이드는 총 3개.
+var currentSlide = Number($('.slider__slide--active').data('slide')), // Number() - 문자열을 숫자로 변환하는 함수. : 현재 슬라이드의 페이지.
+    totalSlides = $('.slider__slide').length, // 전체 슬라이드는 총 3페이지.
     sSlides = $('.slider__slide'),
     sIndicators = $('.slider__indicators'),
     sWraps = $('.slider__wrap'),
     goNext = $('.go_next'); // Next ----> 부분을 담고 있는 class.
+
 
 
 $(document).ready(function(){
@@ -19,15 +20,21 @@ $(document).ready(function(){
       sWraps.addClass('slider__wrap--hacked');
     }, 1000);
   });
+
+  console.log(currentSlide); // 첫번째 슬라이드 확인.
   
 // 슬라이드 이동을 위한 goToSlide 함수 선언.
 function goToSlide(number){
+  console.log(currentSlide); // 2,3페이지로의 슬라이드 이동 확인.
+  // 슬라이드 이동시 기존 슬라이드의 slider__slide--active 클래스를 삭제해주고 이동된 슬라이드의 data-slide number 값을 찾아주어 해당 슬라이드에 slider__slider--active 클래스를 추가해줌으로써 스타일을 변경해준다.
   sSlides.removeClass('slider__slide--active');
   $('.slider__slide[data-slide='+number+']').addClass('slider__slide--active');
+  
 }
 
 // goNext 클래스를 클릭했을 때 현재 슬라이드의 data값이 올라간다.
 goNext.on('click', function(){
+  
   currentSlide++;
   // 만약 현재 슬라이드가 전체 슬라이드 (3개)보다 커질 경우 현재 슬라이드의 값을 다시 1로 바꿔주어 3페이지의 next 는 다시 1페이지가 되도록 한다.
   if (currentSlide > totalSlides){
--- js/script_v.js
+++ js/script_v.js
@@ -2,26 +2,42 @@
 
 // 각 변수들을 선언.
 var navUserAgent = navigator.userAgent,
-    innerVideo = document.querySelectorAll(".active > div > video"),
-    carousel = $('#carousel');
+    innerVideo = document.querySelectorAll(".active > div > video"), // active클래스 내부 div 태그 내부의 video 태그.
+    carousel = $('#carousel'); // carousel id값을 가진 요소.
 
 
     //userAgent에 대한 공부. Chrome, Mozila(Firefox) ...
+    // isChrome, isChromeMobile, isSamsungBrowser, isWindows, isEdge, isIE 등.
+    // test() - 찾는 문자열이 들어있는지, 아닌지를 알려준다.
+    // var navUserAgent = navigator.userAgent 가 있으면 innerVideo속의 태그를 실행. 
 if(!/isChrome/i.test(navUserAgent)) {
+    console.log(navUserAgent); // userAgent 확인.
+    // $(innerVideo).play(); 의 식으로 실행했을 때 찾을 수 없다는 오류가 발생한다.
+    // get(0) 을 중간에 붙여 아래와 같이 식을 작성했더니 정상 실행이 되었다. 
     $(innerVideo).get(0).play();
+    console.log("영상play"); // 영상 재생 확인.
+    // console.log(innerVideo); 
+    // chrome에서 바로 실행이 안되는 것이 autoplay 관련 정책때문인건지?
   
     // on과 bind : 이벤트 처리를 간단하게 할 수 있다. 둘의 기능도 동일하고 사용하는 형태도 동일하지만 jQuery 1.7 이후부터는 on을 사용하도록 권장하고 있다.
     carousel.on('slide.bs.carousel', function(e) {
-        // Video 태그를 찾아주고 컨트롤 및 플레이. 
-        // $(e.relatedTarget).find('video').play(); 의 식으로 실행했을 때 찾을 수 없다는 오류가 발생한다.
-        // get(0) 을 중간에 붙여 아래와 같이 식을 작성했더니 정상 실행이 되었다. 
+      // e.preventDefault();
+
+      // Video 태그를 찾아주고 컨트롤 및 플레이. 
+      // $(e.relatedTarget).find('video').play(); 의 식으로 실행했을 때 찾을 수 없다는 오류가 발생한다.
+      // get(0) 을 중간에 붙여 아래와 같이 식을 작성했더니 정상 실행이 되었다. 
+      // e.relatedTarget - 해당 이벤트와 관련된 다른 DOM 요소를 선택.
       $(e.relatedTarget).find('video').get(0).play();
     });
   
     // 슬라이드를 넘겼을 때 이전 비디오 일시정지.
     carousel.on('slid.bs.carousel', function(e) {
+      // not()은 선택한 요소 중 특정 선택자를 제외한 요소를 선택합니다. 
+      // 현재 슬라이드의 video태그가 아닌 요소들을 전부 선택하여 pause.
+
       $('video').not('.active > div > video').each(function() {
         $(this).get(0).pause();
       });
     });
+    console.log(innerVideo);
   }
