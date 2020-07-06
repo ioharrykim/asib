c3b606f25f7752de780bc1806a063764f018ec20
Hyunmin Kim <landiajuve@gmail.com>
2020-07-06 09:57:48 +0900

    0706수정

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
+// 이미지 슬라이드
+
+// 각 변수들을 선언.
+var currentSlide = Number($('.slider__slide--active').data('slide')),
+    totalSlides = $('.slider__slide').length, // 전체 슬라이드는 총 3개.
+    sSlides = $('.slider__slide'),
+    sIndicators = $('.slider__indicators'),
+    sWraps = $('.slider__wrap'),
+    goNext = $('.go_next'); // Next ----> 부분을 담고 있는 class.
+
+
 $(document).ready(function(){
-    for (var i=1; i <= $('.slider__slide').length; i++){
-      $('.slider__indicators').append('<div class="slider__indicator" data-slide="'+i+'"></div>')
+  // data-slide 3까지 존재.
+  for (var i=1; i <= sSlides.length; i++) {
+    sIndicators.append('<div class="slider__indicator" data-slide="'+i+'"></div>');
     }
-    setTimeout(function(){
-      $('.slider__wrap').addClass('slider__wrap--hacked');
+    // 화면 전환시 백그라운드
+    setTimeout(function() {
+      sWraps.addClass('slider__wrap--hacked');
     }, 1000);
-  })
+  });
   
-  function goToSlide(number){
-    $('.slider__slide').removeClass('slider__slide--active');
-    $('.slider__slide[data-slide='+number+']').addClass('slider__slide--active');
+// 슬라이드 이동을 위한 goToSlide 함수 선언.
+function goToSlide(number){
+  sSlides.removeClass('slider__slide--active');
+  $('.slider__slide[data-slide='+number+']').addClass('slider__slide--active');
+}
+
+// goNext 클래스를 클릭했을 때 현재 슬라이드의 data값이 올라간다.
+goNext.on('click', function(){
+  currentSlide++;
+  // 만약 현재 슬라이드가 전체 슬라이드 (3개)보다 커질 경우 현재 슬라이드의 값을 다시 1로 바꿔주어 3페이지의 next 는 다시 1페이지가 되도록 한다.
+  if (currentSlide > totalSlides){
+    currentSlide = 1;
   }
-  
-  $('.slider__next, .go-to-next').on('click', function(){
-    var currentSlide = Number($('.slider__slide--active').data('slide'));
-    var totalSlides = $('.slider__slide').length;
-    currentSlide++
-    if (currentSlide > totalSlides){
-      currentSlide = 1;
-    }
-    goToSlide(currentSlide);
-  })
+  // goToSlide 함수 실행. 멤버변수로 currentSlide를 준다.
+  goToSlide(currentSlide);
+})
+
+
+
   
 
 // 음악
@@ -64,21 +82,4 @@ function stop3() {
   console.log("mouseleave");
   var music3 = document.getElementById("audio3");
   music3.pause();
-}
-
-
-// 비디오
-
-if(!/iPhone/i.test(navigator.userAgent)) {
-  $('.active > div > video').get(0).play();
-
-  $('#carousel').bind('slide.bs.carousel', function(e) {
-    $(e.relatedTarget).find('video').get(0).play();
-  });
-
-  $('#carousel').bind('slid.bs.carousel', function(e) {
-    $('video').not('.active > div > video').each(function() {
-      $(this).get(0).pause();
-    });
-  });
 }
--- js/script_v.js
+++ js/script_v.js
@@ -1,13 +1,25 @@
 // 비디오
 
-if(!/iPhone/i.test(navigator.userAgent)) {
-    $('.active > div > video').get(0).play();
+// 각 변수들을 선언.
+var navUserAgent = navigator.userAgent,
+    innerVideo = document.querySelectorAll(".active > div > video"),
+    carousel = $('#carousel');
+
+
+    //userAgent에 대한 공부. Chrome, Mozila(Firefox) ...
+if(!/isChrome/i.test(navUserAgent)) {
+    $(innerVideo).get(0).play();
   
-    $('#carousel').bind('slide.bs.carousel', function(e) {
+    // on과 bind : 이벤트 처리를 간단하게 할 수 있다. 둘의 기능도 동일하고 사용하는 형태도 동일하지만 jQuery 1.7 이후부터는 on을 사용하도록 권장하고 있다.
+    carousel.on('slide.bs.carousel', function(e) {
+        // Video 태그를 찾아주고 컨트롤 및 플레이. 
+        // $(e.relatedTarget).find('video').play(); 의 식으로 실행했을 때 찾을 수 없다는 오류가 발생한다.
+        // get(0) 을 중간에 붙여 아래와 같이 식을 작성했더니 정상 실행이 되었다. 
       $(e.relatedTarget).find('video').get(0).play();
     });
   
-    $('#carousel').bind('slid.bs.carousel', function(e) {
+    // 슬라이드를 넘겼을 때 이전 비디오 일시정지.
+    carousel.on('slid.bs.carousel', function(e) {
       $('video').not('.active > div > video').each(function() {
         $(this).get(0).pause();
       });
